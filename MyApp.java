b77b5db3d81e53cbd75e46916d2deff5dc858cb1
uchidarei <goldendoritos@me.com>
2018-10-04 00:12:31 -0700

    add MyApp.java

---
+++ MyApp.java
@@ -0,0 +1,15 @@
+public class MyApp {
+
+  // comment
+  /*
+    comment
+    comment
+    comment
+    comment
+  */
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
